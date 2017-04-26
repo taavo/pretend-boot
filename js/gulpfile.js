f1b12f61cbaeb0ec56cc349a3558cc014f64995b
Taavo Smith <tsmith@pivotal.io>
2017-04-26 14:54:53 -0400

    npm test should run tests headlessly

--- js/gulpfile.js
+++ js/gulpfile.js
@@ -15,6 +15,22 @@ gulp.task('test', function() {
     const plugin = new JasminePlugin();
 
     const jasmineWebpackConfig = _.merge({
+        output: {filename: 'spec.js'},
+    }, webpackConfig);
+
+    jasmineWebpackConfig.plugins.push(plugin);
+    delete(jasmineWebpackConfig.entry);
+
+    return gulp.src(jasmineSources)
+        .pipe(webpackStream(jasmineWebpackConfig, webpack))
+        .pipe(jasmineBrowser.specRunner({console:true}))
+        .pipe(jasmineBrowser.headless());
+});
+
+gulp.task('test:server', function() {
+    const plugin = new JasminePlugin();
+
+    const jasmineWebpackConfig = _.merge({
         watch: true,
         output: {filename: 'spec.js'},
     }, webpackConfig);
